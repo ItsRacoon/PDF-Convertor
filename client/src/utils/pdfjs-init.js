1a5db44aaa4158fa92d001a764ca9e727fe0ab9b
ItsRacoon <visheshkumar632@gmail.com>
2025-05-09 20:26:11 +0530

    last

--- client/src/utils/pdfjs-init.js
+++ client/src/utils/pdfjs-init.js
@@ -1,7 +1,10 @@
-import * as pdfjsLib from 'pdfjs-dist';
+// In your JavaScript where you initialize PDF.js
+import * as pdfjs from 'pdfjs-dist';
 
+// Change this line
+pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.js';  // Use the file you already copied
 // Set up the worker for PDF.js
 // Use a local worker file to avoid CORS issues
-pdfjsLib.GlobalWorkerOptions.workerSrc = '/pdf.worker.js';
+
 
 export default pdfjsLib;
